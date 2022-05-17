73372f99f6aec10421490c0926094d1410384caa
Денис <matyushkin_d@list.ru>
2022-05-18 01:08:34 +0300

    update

---
+++ C++/Семинар/seminar4_1.cpp
@@ -0,0 +1,83 @@
+﻿// Очередь
+
+#include<iostream>
+#include<fstream>
+
+using namespace std;
+
+struct NodeQ {
+	int nb;
+	NodeQ* next;
+};
+NodeQ* pbeg;
+
+// Добавление
+void push(NodeQ** ppbeg, int n) {
+	NodeQ* pv = new NodeQ;
+	pv->nb = n;
+	pv->next = 0;
+	if (*ppbeg) {
+		NodeQ* temp = *ppbeg;
+		while (temp->next) temp = temp->next;
+		temp->next = pv;
+	}
+	else *ppbeg = pv;
+}
+
+// Удаление
+int pop(NodeQ** ppbeg) {
+	NodeQ* pv = *ppbeg;
+	if (*ppbeg == 0) {
+		return -111;
+	}
+	else {
+		int a = pv->nb;
+		*ppbeg = pv->next;
+		delete pv;
+		return a;
+	}
+}
+
+// Просмотр первого
+
+int peek(NodeQ* pbeg) {
+	NodeQ* pv = pbeg;
+	if (pbeg == 0) {
+		return -111;
+	}
+	else {
+		return pv->nb;
+	}
+}
+
+void print(NodeQ* pbeg) {
+	bool a = false;
+	NodeQ* pv = pbeg;
+	while (pv) {
+		cout << pv->nb << " ";
+		pv = pv->next;
+		a = true;
+	}
+	if (a == false) {
+		cout << "Список пустой!\n";
+	}
+}
+
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	NodeQ* pbeg = 0;
+	int k, n;
+	while (1) {
+		cout << "\nЗадайте вариант действия : \n 1 - добавить\n 2 - просмотр первого\n 3 - удалить из очереди\n 4 - распечатка\n другое число - удаление\n";
+		cin >> k;
+		switch (k) {
+		case 1: cout << "Введите номер трамвая: "; cin >> n; push(&pbeg, n); break;
+		case 2: cout << peek(pbeg) << endl; break;
+		case 3: cout << pop(&pbeg) << endl; break;
+		case 4: print(pbeg); break;
+		default: return 0;
+		}
+	}
+}
---
+++ C++/Семинар/seminar4_2.cpp
@@ -0,0 +1,82 @@
+﻿// Стек
+
+#include<iostream>
+
+using namespace std;
+
+struct NodeQ {
+	int nb;
+	NodeQ* next;
+};
+NodeQ* pbeg1, * pbeg2;
+
+// Добавление
+void push(NodeQ** ppbeg, int n) {
+	NodeQ* pv = new NodeQ;
+	pv->nb = n;
+	if (!*ppbeg) {
+		pv->next = 0;
+		*ppbeg = pv;
+	}
+	else {
+		pv->next = *ppbeg;
+		*ppbeg = pv;
+	}
+}
+
+// Удаление
+int pop(NodeQ** ppbeg) {
+	NodeQ* pv = *ppbeg;
+	int a = pv->nb;
+	*ppbeg = pv->next;
+	delete pv;
+	return a;
+}
+
+// Вывод
+void print(NodeQ* pbeg) {
+	bool a = false;
+	NodeQ* pv = pbeg;
+	while (pv) {
+		cout << pv->nb << " ";
+		pv = pv->next;
+		a = true;
+	}
+	if (a == false) {
+		cout << "Список пустой!\n";
+	}
+}
+
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	NodeQ* pbeg1 = 0, * pbeg2 = 0;
+	int k, n, w;
+	while (1) {
+		cout << "\nЗадайте вариант действия : \n 1 - добавить\n 2 - удаление\n 3 - печать\n другое число - удаление\n";
+		cin >> k;
+		switch (k) {
+		case 1: cout << "Введите номер трамвая: "; cin >> n; push(&pbeg1, n); break;
+		case 2: {
+			cout << "Введите номер трамвая для удаления: ";
+			cin >> n;
+			while (1) {
+				if (!pbeg1) {
+					cout << "Вагона нет!\n";
+					break;
+				}
+				w = pop(&pbeg1);
+				if (w == n) break;
+				push(&pbeg2, w);
+			}
+			while (pbeg2) {
+				push(&pbeg1, pop(&pbeg2));
+			}
+			break;
+		}
+		case 3: print(pbeg1); break;
+		default: return 0;
+		}
+	}
+}
